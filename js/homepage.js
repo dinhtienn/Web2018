1c60396bd9012caa9c845e68e371339cc9e7a8ea
DESKTOP-LG083FR\Dinh Tien <dinhtiennguyen.1202@gmail.com>
2018-12-28 12:56:27 +0700

    mobile-raw

--- js/homepage.js
+++ js/homepage.js
@@ -3,14 +3,18 @@ var layerOpacity = document.getElementsByClassName("layer_opacity")[0];
 var subMenu = document.getElementsByClassName("sub-menu");
 for (i = 0; i < subMenu.length; i++) {
     subMenu[i].onmouseover = function() {
-        layerOpacity.style.height = document.getElementsByTagName('body')[0].clientHeight + "px";
+        if (window.outerWidth > 768) {
+            layerOpacity.style.height = document.getElementsByTagName('body')[0].clientHeight + "px";
+        }
     };
     subMenu[i].onmouseout = function() {
-        layerOpacity.style.height = "0px";
+        if (window.outerWidth > 768) {
+            layerOpacity.style.height = "0px";
+        }
     }
 }
 
-// Color iconSearch when focusing searchBar
+// iconSearch change color and searchBar scale when focusing searchBar
 var searchField = document.getElementById("search");
 var iconSearch = document.getElementsByTagName("i")[0];
 searchField.onfocus = function() {
@@ -20,4 +24,26 @@ searchField.onfocus = function() {
 searchField.onblur = function () {
     iconSearch.style.color = "#7088a9";
     iconSearch.style.transform = "scale(1)";
+}
+
+// Mobile: Menu onClick
+var menuButton = document.getElementById("icon-nav");
+let navTag = document.getElementsByTagName("nav")[0];
+var navMobileContainer = document.getElementsByClassName("nav-mobile-container")[0];
+var closeButton = document.getElementsByClassName("close-nav-mobile")[0];
+
+menuButton.onclick = function() {
+    navTag.style.width = "70%";
+    setTimeout(function(){
+        navMobileContainer.style.display = "block";
+        layerOpacity.style.width = "100%";
+    }, 100);
+    layerOpacity.style.height = document.getElementsByTagName('body')[0].clientHeight + "px";
+}
+
+console.log(closeButton);
+closeButton.onclick = function() {
+    navMobileContainer.style.display = "none";
+    navTag.style.width = "0%";
+    layerOpacity.style.height = "0px";
 }
