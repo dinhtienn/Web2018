1d343e4f7f5e7af96b59c4b94f269ec8ca6d11e1
DESKTOP-LG083FR\Dinh Tien <dinhtiennguyen.1202@gmail.com>
2019-01-25 01:43:03 +0700

    delete module testuser

--- connectDB.php
+++ connectDB.php
@@ -7,6 +7,7 @@
     $conn = mysqli_connect($hostname, $username, $password, $dbname);
     if (!$conn) {
         echo "Kết nối Database không thành công";
+        die($conn->connect_error);
     } else {
         mysqli_set_charset($conn,"utf8");
     }
--- testUser/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['username'])) { ?>
-        <a href="managePost.php"><button>Quản lý bài viết</button></a>
-        <a href="createPost.php"><button>Đăng bài viết</button></a>
-        <a href="logout.php"><button>Đăng xuất</button></a>
-    <?php } else {
-        header('location:login.php');
-}?>
